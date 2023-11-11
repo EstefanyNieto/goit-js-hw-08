cebb508459964f42a2c26819566014c5bf8154aa
Estefany Nieto <119767197+EstefanyNieto@users.noreply.github.com>
2023-11-10 22:56:20 -0500

    new code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Described in documentation;
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+// Additional styles import
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryList = document.querySelector('.gallery');
+const createGallery = el => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
+    })
+    .join('');
+};
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', photosMarkup);
+// --------------------------------------------------------------
+const gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+gallery.on('show.simplelightbox');
